3f60cfbd75a8eba0b84ac5eed66116a8827432c4
Puskin <mbpuskin@gmail.com>
2012-03-24 16:42:27 +0100

    Fixed datepicker adding for task update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,8 +23,7 @@ $(document).ready(function(ev){
 	}); 
 
 	$(function() {
-		$("#new_task_form #task_due_date").datepicker({dateFormat: 'yy-mm-dd', firstDay: 1});
-		$(".modal-body #task_due_date").datepicker({dateFormat: 'yy-mm-dd', firstDay: 1});
+		$("#task_due_date").datepicker({dateFormat: 'yy-mm-dd', firstDay: 1});
 	});
 
 
